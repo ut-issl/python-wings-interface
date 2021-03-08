91ddfdbc0fc58323e413147ac18a07535e7c0b38
Kanta Yanagida <yanagida@space.t.u-tokyo.ac.jp>
2021-03-08 15:59:59 +0900

    update tlm/cmd interface

--- isslwings/operation.py
+++ isslwings/operation.py
@@ -3,6 +3,8 @@
 
 import requests
 
+from .telemetry import Telemetry
+
 
 class Operation:
     def __init__(self, operation_idx: int, url: str = "http://localhost:5000"):
@@ -11,37 +13,35 @@ class Operation:
         response = requests.get("{}/api/operations".format(self.url)).json()
         self.operation_id = response["data"][operation_idx]["id"]
 
-    def send_set_block_pos(self, bc_idx: str, cmd_idx) -> bool:
+    def send_set_block_pos(self, bc_idx: int, cmd_idx: int) -> None:
         cmd_code = "0x000e"
         cmd_params = [
-            {"type": "uint16", "value": bc_idx},
-            {"type": "uint8", "value": cmd_idx},
+            {"type": "uint16", "value": str(bc_idx)},
+            {"type": "uint8", "value": str(cmd_idx)},
         ]
-        return self._send_cmd(cmd_code, cmd_params)
+        self._send_cmd(cmd_code, cmd_params)
 
-    def send_generate_tlm(self, tlm_id: str) -> bool:
+    def send_generate_tlm(self, tlm_id: int) -> None:
         cmd_code = "0x0021"
         cmd_params = [
             {"type": "uint8", "value": "0x40"},
-            {"type": "uint8", "value": tlm_id},
+            {"type": "uint8", "value": str(tlm_id)},
             {"type": "uint8", "value": "1"},
         ]
-        return self._send_cmd(cmd_code, cmd_params)
+        self._send_cmd(cmd_code, cmd_params)
 
-    def get_latest_tlm(self, tlm_name: str) -> list:
+    def get_latest_tlm(self, tlm_code_name: str) -> Telemetry:
         response = requests.get(
             "{}/api/operations/{}/tlm".format(self.url, self.operation_id)
         ).json()
-        for resp_data in response["data"]:
-            if resp_data["packetInfo"]["name"] == tlm_name:
-                tlm = resp_data["telemetries"]
-        return tlm
+        return Telemetry(tlm_code_name, response)
 
-    def _send_cmd(self, cmd_code: str, cmd_params: list) -> bool:
+    def _send_cmd(self, cmd_code: str, cmd_params: list) -> None:
         json_data = {"command": {"code": cmd_code, "params": cmd_params}}
         response = requests.post(
             "{}/api/operations/{}/cmd".format(self.url, self.operation_id),
             json=json_data,
         ).json()
 
-        return response["ack"]
+        if response["ack"] == False:
+            raise Exception("Error has occured while sending {}".format(cmd_code))
