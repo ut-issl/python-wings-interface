e6e674be2b1a9c3e4ad13e12cbc192289d111b7b
Kanta Yanagida <yanagida@space.t.u-tokyo.ac.jp>
2021-06-15 17:42:32 +0900

    update __init__.py

--- isslwings/__init__.py
+++ isslwings/__init__.py
@@ -1,2 +1,6 @@
 from .operation import Operation
-from .util import generate_and_receive_tlm, send_cmd, send_cmd_and_confirm
+from .util import (
+    generate_and_receive_tlm,
+    send_bl_cmd_and_confirm,
+    send_rt_cmd_and_confirm,
+)
